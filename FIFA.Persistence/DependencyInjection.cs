397bd8b0cb6404887962d80a53a3575790a92825
Kostiantyn Logunov <777koc@gmail.com>
2023-09-15 23:11:39 +0300

    added prepering DI

---
+++ FIFA.Persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using FIFA.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FIFA.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var connectionString = configuration["DbConnection"];
+
+            services.AddDbContext<FootballersDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+
+            services.AddScoped<IFootballersDbContext>(provider => provider.GetService<FootballersDbContext>());
+
+            return services;
+        }
+    }
+}
